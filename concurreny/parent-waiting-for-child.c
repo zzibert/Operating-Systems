3973e7214bd77d93915bcf3caff9db71f0532af7
Žan Žibert <zzibert@plume.com>
2024-04-01 11:08:54 +0200

    Parent waiting for Child: Use a Condition Variable

--- concurreny/parent-waiting-for-child.c
+++ concurreny/parent-waiting-for-child.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <assert.h>
+
 #include "common_threads.h"
 
-volatile int done = 0;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;
+
+int done = 0;
+
+void thr_exit() {
+    Pthread_mutex_lock(&m);
+    done = 1;
+    pthread_cond_signal(&c);
+    Pthread_mutex_unlock(&m);
+}
 
 void *child(void *arg) {
     printf("child\n");
-    done = 1;
+    thr_exit();
     return NULL;
+}
+
+void thr_join() {
+    Pthread_mutex_lock(&m);
+    while (done == 0) pthread_cond_wait(&c, &m);
+    Pthread_mutex_unlock(&m);
 }
 
 int main(int argc, char *argv[]) {
     printf("parent: begin\n");
-    pthread_t c;
-    Pthread_create(&c, NULL, child, NULL); // create child
-    while (done == 0); // spin
+    pthread_t p;
+    Pthread_create(&p, NULL, child, NULL); // create child
+    thr_join();
     printf("parent: end\n");
     return 0;
 }
