d4c7022203f391da2bdc1a5f9177f8b6ed5636f6
Žan Žibert <zzibert@plume.com>
2024-04-01 11:46:04 +0200

    Producer/Consumer threads

---
+++ concurreny/producer-consumer-threads.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#include "common_threads.h"
+
+int buffer;
+int count = 0; // initially, empty
+
+void put(int value) {
+    assert(count == 0);
+    count = 1;
+    buffer = value;
+}
+
+int get() {
+    assert(count == 1);
+    count = 0;
+    return buffer;
+}
+
+void *producer(void *arg) {
+    int loops = (int) arg;
+    for (int i = 0; i < loops; i++) {
+        put(i);
+    }
+}
+
+void *consumer(void *arg) {
+    int i;
+    while (1) {
+        int tmp = get();
+        printf("%d\n", tmp);
+    }
+}
