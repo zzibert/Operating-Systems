834e4880c068aaa2fda17ae8c4fee4b33c1078f5
Žan Žibert <zzibert@plume.com>
2024-04-01 13:22:24 +0200

    The Correct Producer/Consumer Synchronization

--- concurreny/producer-consumer-threads.c
+++ concurreny/producer-consumer-threads.c
@@ -9,28 +9,33 @@
 pthread_cond_t empty, full;
 pthread_mutex_t m;
 
-
-int buffer;
-int count = 0; // initially, empty
-int loops = 10;
+int loops = 1000;
+const int MAX = 1000;
+int buffer[MAX];
+int fill_ptr = 0;
+int use_ptr = 0;
+int count = 0;
 
 void put(int value) {
-    assert(count == 0);
-    count = 1;
-    buffer = value;
+    buffer[fill_ptr] = value;
+    fill_ptr = (fill_ptr + 1) % MAX;
+    count++;
 }
 
 int get() {
-    assert(count == 1);
-    count = 0;
-    return buffer;
+    int temp = buffer[use_ptr];
+    use_ptr = (use_ptr + 1) % MAX;
+    count--;
+    return temp;
 }
+
+
 
 void *producer(void *arg) {
     for (int i = 0; i < loops; i++) {
         pthread_mutex_lock(&m);                         // p1
-        while (count == 1)                              // p2
-            pthread_cond_wait(&empty, &m);               // p3
+        while (count == MAX)                             // p2
+            pthread_cond_wait(&empty, &m);              // p3
         put(i);                                         // p4
         pthread_cond_signal(&full);                     // p5
         pthread_mutex_unlock(&m);                       // p6
@@ -43,7 +48,7 @@ void *consumer(void *arg) {
         while (count == 0)                              // c2
             pthread_cond_wait(&full, &m);               // c3
         int tmp = get();                                // c4
-        pthread_cond_signal(&empty);                     // c5
+        pthread_cond_signal(&empty);                    // c5
         pthread_mutex_unlock(&m);                       // c6
         printf("%d\n", tmp); 
     }
