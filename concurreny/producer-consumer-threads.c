86dd1def583bee34e27067930bdbcd6af2678acd
Žan Žibert <zzibert@plume.com>
2024-04-01 12:10:44 +0200

    Producer/Consumer: Single CV and If Statement

--- concurreny/producer-consumer-threads.c
+++ concurreny/producer-consumer-threads.c
@@ -6,8 +6,13 @@
 
 #include "common_threads.h"
 
+pthread_cond_t cond;
+pthread_mutex_t m;
+
+
 int buffer;
 int count = 0; // initially, empty
+int loops = 10;
 
 void put(int value) {
     assert(count == 0);
@@ -22,16 +27,22 @@ int get() {
 }
 
 void *producer(void *arg) {
-    int loops = (int) arg;
     for (int i = 0; i < loops; i++) {
+        pthread_mutex_lock(&m);
+        if (count == 1) pthread_cond_wait(&cond, &m);
         put(i);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&m);
     }
 }
 
 void *consumer(void *arg) {
-    int i;
-    while (1) {
+    for (int i = 0; i < loops; i++) {
+        pthread_mutex_lock(&m);
+        if (count == 0) pthread_cond_wait(&cond, &m);
         int tmp = get();
         printf("%d\n", tmp);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&m);
     }
 }
