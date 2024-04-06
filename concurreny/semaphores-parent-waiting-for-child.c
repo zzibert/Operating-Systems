8983a7eb035bca43d645308e337737851f662299
Žan Žibert <zzibert@plume.com>
2024-04-06 13:03:18 +0200

    Semaphores: Parent waiting for child 2nd attempt

--- concurreny/semaphores-parent-waiting-for-child.c
+++ concurreny/semaphores-parent-waiting-for-child.c
@@ -1,26 +1,32 @@
-#include <assert.h>
-#include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <unistd.h>
+
 #include "common.h"
 #include "common_threads.h"
+
+#ifdef linux
+#include <semaphore.h>
+#elif __APPLE__
+#include "zemaphore.h"
+#endif
 
 sem_t s;
 
 void *child(void *arg) {
+    sleep(2);
     printf("child\n");
-    sem_post(&s); // signal here: child is done
+    Sem_post(&s); // signal here: child is done
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    sem_init(&s, 0, 1); // what should X be?
+    Sem_init(&s, 0); 
     printf("parent: begin\n");
     pthread_t c;
     Pthread_create(&c, NULL, child, NULL);
-    sem_wait(&s); // wait here for child
+    Sem_wait(&s); // wait here for child
     printf("parent: end\n");
     return 0;
 }
